deae6066019c981dcb15ba463e227d5ac4cd49e0
White Devil <88606803+white-devil-gits@users.noreply.github.com>
2021-08-11 13:14:52 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -18,13 +18,9 @@ Asena.addCommand({pattern: 'owner', fromMe: false, desc: Lang.WP}, (async (messa
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Vishnu Mkz*
-*owner number wa.me/916235989299*
-*instagram  : https://www.instagram.com/ig.zero.x2k_linda?r=nametag*
-*bot setting video : https://youtu.be/_D4ZYuUSXjs
-*githublink       https://github.com/vishnu-memanakizhakathil/Meenakshi*
-*audio commads    https://github.com/vishnu-memanakizhakathil/Meenakshi/tree/master/uploads*
-*sticker commads  https://github.com/vishnu-memanakizhakathil/Meenakshi/tree/master/uploads*
-`}) 
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*VediKettu Jaanu   Coded by White Devil*
+*owner number wa.me/56952224440*
+
+*githublink       https://github.com/white-devil-gits/VEDIKETTU-JAANU-2.0*
 
 }));
